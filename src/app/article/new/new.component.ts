b0e45fd486d3b0df592cc3893e8fed2589cb5893
이강호 <cannalee90@gmail.com>
2018-07-13 16:12:42 +0900

    article.id 할당

--- src/app/article/new/new.component.ts
+++ src/app/article/new/new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { select } from '@angular-redux/store';
+import { Observable, select } from 'redux-observable';
 
 import { ArticleActions } from '../article.actions';
 import { IArticle, Article } from '../../models';
@@ -11,16 +13,25 @@ import { IArticle, Article } from '../../models';
 export class NewComponent implements OnInit {
 
   model: IArticle = new Article(0, '', '', '', new Date(), new Date());
+  @select() readonly articles$: Observable<IArticle[]>;
 
   constructor(
     private actions: ArticleActions
   ) { }
 
   ngOnInit() {
+    this.actions.fetchArticles();
+    this.articles$
+      .take(1)
+      .subscribe((articles: IArticle[]) => {
+        const nextId = (articles.length === 0) ? 1 : articles[articles.length - 1].id + 1;
+        this.model.id = nextId;
+      });
   }
 
   onSubmit() {
     this.actions.postArticle(this.model);
   }
+
 
 }
