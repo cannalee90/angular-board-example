568b002c0029ea5cc27fdd2caf1f5bbef5e989d5
이강호 <kangho@bluehole.net>
2018-07-25 15:46:36 +0900

    filtered article 적용하기

--- src/app/article/list/list.component.ts
+++ src/app/article/list/list.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { NgRedux, select } from '@angular-redux/store';
 
 import { ArticleActions } from '../article.actions';
+import { getContainDArticle } from '../../store/index';
 import { IAppState } from '../../store';
 import { IArticle } from '../../models';
 
@@ -13,11 +14,13 @@ import { IArticle } from '../../models';
 })
 export class ListComponent implements OnInit {
   @select() readonly articles$: Observable<IArticle[]>;
-
+  @select(getContainDArticle) readonly filteredArticles$: Observable<IArticle[]>;
+  articleLength: number = 0;
+  filteredArticleLength: number = 0;
   constructor(
     private ngRedux: NgRedux<IAppState>,
     private actions: ArticleActions
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.actions.fetchArticles();
